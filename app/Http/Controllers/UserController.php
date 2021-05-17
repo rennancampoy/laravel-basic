800ecc8bc2c397ff5be306bedd812cac2a98e393
rennancampoy <rennancampoy@yahoo.com.br>
2021-05-17 04:14:31 -0300

    creating empty user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public Request $request = new Request();
+    
+    public function __construct()
+    {
+        
+    }    
+    
+}
