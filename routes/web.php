99e719994b34a36c2c5c0b08c6138610933e87be
rennancampoy <rennancampoy@yahoo.com.br>
2021-05-17 04:14:15 -0300

    returning to home page if route doesnt exists

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('', function () {
     return view('welcome');
+})->name('home');
+
+Route::fallback(function() {
+    return redirect()->route('home');
 });
